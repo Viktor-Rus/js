82012f806500c2008554f465fed79c2286aa1ddb
Viktor <crimeacriminal@gmail.com>
2020-04-12 14:38:11 +0300

    first project

--- myjavascript.js
+++ myjavascript.js
@@ -1,24 +1,21 @@
-let money = +prompt("Ваш бюджет на месяц?"),
-    time= prompt("Введите дату в формате YYYY-MM-DD");
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let a =prompt("Введите обязательную статью расходов в этом месяце");
-let b =+prompt("Во сколько обойдется?");
-
-
-let appDate = {
-    money,
-    time,
-    expenses: {
-        a,
-        b
-    },
-    optionalExpenses: {},
-    income: [],
-    savings: false
-    
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-let c = (money - b)/30;
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-alert(c);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
+alert(appData.budget / 30);
